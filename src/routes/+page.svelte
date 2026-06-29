6f83a44c05e942c412e8258aa21a49b00083b9ed
liande <liande@guocn.org>
2026-06-29 20:17:33 +0200

    fix 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import { m, meta_desc } from '$lib/paraglide/messages';
-	import { gsap } from 'gsap';
+
 	import Cursix from 'cursix';
 	import Nav from '$lib/Nav.svelte';
 	import Footer from '$lib/Footer.svelte';
@@ -14,6 +14,7 @@
 	import Contact from '$lib/sections/Contact.svelte';
 	// Cursor
 	onMount(async () => {
+		const { gsap } = await import('gsap');
 		Cursix.registerGSAP(gsap);
 		const hannoverEl = document.getElementById('hannoverEl');
 		const titleEl = document.querySelector('.titleName');
